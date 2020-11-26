3b835210e5f39ae15affd416130e7bf7bbcc36d1
sHOTa-23 <55255354+sHOTa-23@users.noreply.github.com>
2020-11-26 19:08:41 +0400

    added naive approach to Q learning and see why it does not perform well

---
+++ Naive_Deep_Q_Learning/Agent.py
@@ -0,0 +1,45 @@
+import numpy as np
+import torch as T
+from Naive_Deep_Q_Learning.Network import Naive_Q_network
+
+
+class Agent:
+    def __init__(self, input_dim, n_action, lr=0.001, gamma=0.99, eps_max=1.0, eps_min=0.01,
+                 eps_decay=1e-5, ):
+        self.input_dim = input_dim
+        self.n_action = n_action
+        self.lr = lr
+        self.gamma = gamma
+        self.eps = eps_max
+        self.min_eps = eps_min
+        self.eps_decay = eps_decay
+        self.action_space = [i for i in range(self.n_action)]
+
+        self.Q = Naive_Q_network(lr=self.lr, n_action=self.n_action, input_dim=self.input_dim)
+
+    def get_action(self, state):
+        action = np.random.choice(self.action_space)
+        if np.random.random() > self.eps:
+            state = T.tensor(state, dtype=T.float).to(self.Q.device)
+            action = T.argmax(self.Q.forward(state)).item()
+        return action
+
+    def decrease_epsilon(self):
+        self.eps = self.eps - self.eps_decay
+        if self.eps < self.min_eps:
+            self.eps = self.min_eps
+
+    def learn(self, state, action, reward, next_state):
+        self.Q.optimiser.zero_grad()
+        states = T.tensor(state, dtype=T.float).to(self.Q.device)
+        rewards = T.tensor(reward).to(self.Q.device)
+        actions = T.tensor(action).to(self.Q.device)
+        next_states = T.tensor(next_state, dtype=T.float).to(self.Q.device)
+
+        curr_q = self.Q.forward(states)[actions]
+        next_state_value = self.Q.forward(next_states).max()
+        target_q = rewards + self.gamma * next_state_value
+        loss = self.Q.loss(target_q,curr_q).to(self.Q.device)
+        loss.backward()
+        self.Q.optimiser.step()
+        self.decrease_epsilon()
---
+++ Naive_Deep_Q_Learning/Q_Learning.py
@@ -0,0 +1,36 @@
+import gym
+import numpy as np
+from Naive_Deep_Q_Learning.Network import Naive_Q_network
+from Naive_Deep_Q_Learning.Agent import Agent
+from Utils import plot_learning_curve
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v1')
+    n_games = 10000
+    scores = []
+    eps_history = []
+
+    agent = Agent(lr=0.0001, input_dim=env.observation_space.shape,
+                  n_action=env.action_space.n)
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        state = env.reset()
+
+        while not done:
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(action)
+            score += reward
+            agent.learn(state, action, reward, next_state)
+            state = next_state
+        scores.append(score)
+        eps_history.append(agent.eps)
+
+        if i % 100 == 0:
+            avg_score = np.mean(scores[-100:])
+            print('episode ', i, 'score %.1f avg score %.1f epsilon %.2f' %
+                  (score, avg_score, agent.eps))
+    filename = 'cartpole_naive_dqn.png'
+    x = [i + 1 for i in range(n_games)]
+    plot_learning_curve(x, scores, eps_history, filename)
